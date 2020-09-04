6e4951dadd8c6347a3de0e25fb2e8873d792a4b0
Oscar Alfredo Gómez Elizarrarás <elizarrarasalfredoog@gmail.com>
2020-09-04 00:18:59 -0500

    Fetch data from the API to show it in cards.

--- src/index.js
+++ src/index.js
@@ -9,13 +9,31 @@ CardComponent.render(body);
 window.onload = () => {
   AppBarTopComponent.addEvents();
   AppBarTopComponent.getSearchValueEvent((value) => {
-    const card = {
-      id: '1',
-      title: value,
-      description: 'clear sky',
-      temperature: '25°C',
-      icon: '01d',
-    };
-    CardComponent.showCard(card);
+    value = value.trim();
+    if (!value || value === '') return;
+    getWeatherAndDisplayItsCard(value);
   });
 };
+
+async function getWeatherAndDisplayItsCard(cityName) {
+  console.log(cityName);
+  const response = await getWeatherByCityName(cityName);
+  const cityData = await response.json();
+  console.log(cityData);
+  if (cityData.cod === 200) CardComponent.showCard(Card(cityData));
+  else alert(cityData.message);
+}
+
+const Card = (weatherInfo) => {
+  return {
+    id: weatherInfo.id,
+    title: weatherInfo.name,
+    description: weatherInfo.weather[0].description,
+    temperature: weatherInfo.main.temp,
+    icon: weatherInfo.weather[0].icon
+  };
+};
+
+const getWeatherByCityName = (cityName) => fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=384d66935b0b94de64704db43a531d6e`, {mode: 'cors'});
+
+const getWeatherByCoordinates = (lon, lat) => fetch(`api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=384d66935b0b94de64704db43a531d6e`, {mode: 'cors'});
