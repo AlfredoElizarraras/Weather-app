99ba74256e6a9cb72f7e529768f2491e77d7379a
Oscar Alfredo Gómez Elizarrarás <elizarrarasalfredoog@gmail.com>
2020-09-04 22:13:51 -0500

    Add error modal component.

--- src/error-modal/errorModalComponent.js
+++ src/error-modal/errorModalComponent.js
@@ -1,18 +1,28 @@
 import './styles/errorModal.scss';
 import './dom/dom';
-import './events/events';
+import modalWindowClick from './events/events';
+import * as dom from './dom/dom';
 
-const ErrorModalComponent = (() =>{
-  const showError = () => {
+const ErrorModalComponent = (() => {
+  const showError = (cod, message) => {
+    dom.changeInnerHTML(dom.errorTitle(), `Error ${cod}`);
+    dom.changeInnerHTML(dom.errorMessage(), message);
+    dom.showElement(dom.errorModal());
+  };
 
+  const addEvents = () => {
+    modalWindowClick();
   };
 
   const render = (parent) => {
-
+    dom.addToInnerHTML(parent, dom.createMarkUp());
   };
 
   return {
     render,
-    showError
+    addEvents,
+    showError,
   };
-});
+})();
+
+export default ErrorModalComponent;
--- src/index.js
+++ src/index.js
@@ -2,14 +2,18 @@ import './index.css';
 import AppBarTopComponent from './app-bar-top/app-bar-topComponent';
 import CardComponent from './card/cardComponent';
 import Weather from './Models/weather';
+import ErrorModalComponent from './error-modal/errorModalComponent';
 import * as loadBar from './loadBar/loadBar';
 
 const body = document.getElementsByTagName('body')[0];
 AppBarTopComponent.render(body);
 CardComponent.render(body);
+ErrorModalComponent.render(body);
 
 window.onload = () => {
   AppBarTopComponent.addEvents();
+  ErrorModalComponent.addEvents();
+
   AppBarTopComponent.getSearchValueEvent((value) => {
     value = value.trim();
     if (value === '') return;
@@ -18,7 +22,7 @@ window.onload = () => {
     Weather().findByCityName(value).then((newCard) => {
       loadBar.hideBar();
       if (!newCard.error) CardComponent.showCard(newCard);
-      else alert(newCard.error.message);
+      else ErrorModalComponent.showError(newCard.error.cod, newCard.error.message);
     });
   });
 };
